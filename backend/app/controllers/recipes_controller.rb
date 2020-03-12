693479bc964e0affe6483ae898c209d8c49889ae
github username <github email address>
2020-03-12 19:29:01 -0400

    setup crud actions

--- backend/app/controllers/recipes_controller.rb
+++ backend/app/controllers/recipes_controller.rb
@@ -1,2 +1,30 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  def show
+    recipe = Recipe.find_by(params[:id])
+    render json: recipe
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+    render json: recipe
+  end
+
+  def destroy
+    recipe = Recipe.find_by(params[:id])
+    recipe.delete
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :story, :directions, :ingredients,
+                                   :category, :diet, :cook_time, :prep_time,
+                                   :user_id)
+  end
 end
