9e8b82de28e2c6478a79f43ac1cc846912165568
github username <github email address>
2020-03-16 15:56:42 -0400

    added function that returns a render of all recipes

--- frontend/recipe.js
+++ frontend/recipe.js
@@ -13,6 +13,24 @@ class Recipe {
     this.category = category;
     this.user_id = user_id;
 
+
+    this.element = document.createElement('div')
+    this.element.className = 'recipe'
+    this.element.id = `recipe-${this.id}`
+
     Recipe.all.push(this)
   }
+
+  // user(){
+  //  return User.all.find(user => user.id === this.user_id)
+  // }
+
+  fullRender(){
+    this.element.innerHTML = `
+    <h1>${this.name}</h1>
+    <p>${this.story}</p>
+    `
+    return this.element
+  }
+
 }
