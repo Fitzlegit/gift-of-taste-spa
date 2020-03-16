5c87b56f0e25ce5afd15e7ee45785ff9a38a8a00
github username <github email address>
2020-03-15 23:02:34 -0400

    updated attributes

--- backend/app/serializers/recipe_serializer.rb
+++ backend/app/serializers/recipe_serializer.rb
@@ -1,4 +1,8 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :name, :ingredients, :directions,
+             :diet, :category, :cook_time,
+             :prep_time, :story
+
+  belongs_to :user
 end
