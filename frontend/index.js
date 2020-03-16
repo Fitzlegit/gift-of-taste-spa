88e869e82a0dbe12bae4c71328e45b8767605765
github username <github email address>
2020-03-16 15:57:54 -0400

    added event and function to render all recipes

--- frontend/index.js
+++ frontend/index.js
@@ -2,3 +2,30 @@ const recipeAdapter = new RecipesAdapter("http://localhost:3000/recipes")
 
 
 recipeAdapter.fetchRecipes()
+
+
+const menu = document.getElementById('menu')
+const main = document.getElementById('main')
+
+// event  listeners
+menu.addEventListener('click', handleMenuClick)
+
+function handleMenuClick(event){
+  if(event.target.id !== 'menu'){
+    callbacks[`${event.target.id}`]()
+  }
+}
+
+
+const callbacks = {
+  //home: renderHome,
+  //myRecipes: renderMyRecipes,
+  //newRecipes: renderNewRecipes,
+  allRecipes: renderAllRecipes
+}
+
+function renderAllRecipes(){
+  Recipe.all.forEach(recipe => {
+    main.appendChild(recipe.fullRender())
+  })
+}
