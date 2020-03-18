e7c1b5001847f5e42ad1029cd0a2f207258ab892
github username <github email address>
2020-03-18 11:04:26 -0400

    updated routes in recipes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :recipes, only: [:index, :create, :show, :update, :destroy]
   resources :cookbooks
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
