04df7b00a02538e2325e97fc817bc9d8fe79399e
github username <github email address>
2020-03-18 11:15:44 -0400

    added update method

--- frontend/recipesAdapter.js
+++ frontend/recipesAdapter.js
@@ -22,6 +22,18 @@ class RecipesAdapter {
       .then(res => res.json())
       .then((resObj) => this.sanitizeAndAddRecipe(resObj.data))
   }
+
+  //update recipe
+  updateRecipe(recipeObj){
+    let configObj = {
+    method: 'PATCH'
+    headers: {'Content-Type': 'application/json', 'Accepts': 'application/json'},
+    body: JSON.stringify(recipeObj)
+  }
+  fetch(this.baseURL, configObj)
+    .then(res => res.json())
+    .then((resObj) => this.sanitizeAndAddRecipe(resObj.data))
+}
   // sanitizes new recipe
   sanitizeAndAddRecipe(recipeObj){
     let sanitized = {id: recipeObj.id, ...recipeObj.attributes, user_id: recipeObj.relationships.user.data.id}
