9f481ecf09e9ace759fc75b1d5471b0e8d04aa13
github username <github email address>
2020-03-18 11:07:27 -0400

    cleaned up redundant code

--- frontend/recipesAdapter.js
+++ frontend/recipesAdapter.js
@@ -7,11 +7,24 @@ class RecipesAdapter {
     fetch(this.baseURL)
       .then(res => res.json())
       .then(resObj => {
-        resObj.data.forEach(recipeObj => {
-          let sanitized = {id: recipeObj.id, ...recipeObj.attributes, user_id: recipeObj.relationships.user.data.id}
-          new Recipe(sanitized)
-        })
+        resObj.data.forEach(this.sanitizeAndAddRecipe)
       })
-      .then(() => console.log(Recipe.all))
+  }
+
+  //create new Recipe
+  newRecipe(recipeObj){
+    let configObj = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'Accepts': 'application/json'},
+      body: JSON.stringify(recipeObj)
+    }
+    fetch(this.baseURL, configObj)
+      .then(res => res.json())
+      .then((resObj) => this.sanitizeAndAddRecipe(resObj.data))
+  }
+  // sanitizes new recipe
+  sanitizeAndAddRecipe(recipeObj){
+    let sanitized = {id: recipeObj.id, ...recipeObj.attributes, user_id: recipeObj.relationships.user.data.id}
+    new Recipe(sanitized)
   }
 }
